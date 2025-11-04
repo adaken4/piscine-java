ac612cee2f47285225f355883c606744aaa5c55a
kada <adakennedy6@gmail.com>
2025-11-04 15:49:07 +0300

    fix: add Palindrome exercise

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -8,15 +8,6 @@ public class Palindrome {
             char l = s.charAt(left);
             char r = s.charAt(right);
 
-            // Skip non-alphanumeric
-            if (!Character.isLetterOrDigit(l)) {
-                left++;
-                continue;
-            }
-            if (!Character.isLetterOrDigit(r)) {
-                right--;
-                continue;
-            }
             // Case insensitive comparison
             if (Character.toLowerCase(l) != Character.toLowerCase(r)) {
                 return false;
