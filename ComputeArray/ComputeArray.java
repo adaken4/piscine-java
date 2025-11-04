4d9a4494f068c4e161d72a9d2a50409f314b6fcc
kada <adakennedy6@gmail.com>
2025-11-04 16:31:59 +0300

    feat: add ComputeArray exercise

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,22 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        int[] result = new int[array.length];
+        
+        for (int i = 0; i < array.length; i++) {
+            int remainder = array[i] % 3;
+            
+            if (remainder == 0) {
+                // Multiple of 3: multiply by 5
+                result[i] = array[i] * 5;
+            } else if (remainder == 1) {
+                // Multiple of 3 + 1: increment by 7
+                result[i] = array[i] + 7;
+            } else {
+                // Multiple of 3 + 2: stays unchanged
+                result[i] = array[i];
+            }
+        }
+        
+        return result;
+    }
+}
