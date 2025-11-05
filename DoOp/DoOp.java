1337fc8c1050db2bba19292c6aab302821b71b13
kada <adakennedy6@gmail.com>
2025-11-05 11:10:23 +0300

    feat: add DoOp exercise

---
+++ DoOp/DoOp.java
@@ -0,0 +1,38 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args.length != 3) {
+            return "it depends on the input.";
+        }
+
+        String operator = args[1];
+        int num1, num2;
+
+        try {
+            num1 = Integer.parseInt(args[0]);
+            num2 = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            return "it depends on the input.";
+        }
+
+        switch (operator) {
+            case "+":
+                return String.valueOf(num1 + num2);
+            case "-":
+                return String.valueOf(num1 - num2);
+            case "*":
+                return String.valueOf(num1 * num2);
+            case "/":
+                if (num2 == 0) {
+                    return "Error";
+                }
+                return String.valueOf(num1 / num2);
+            case "%":
+                if (num2 == 0) {
+                    return "Error";
+                }
+                return String.valueOf(num1 % num2);
+            default:
+                return "Error";
+        }
+    }
+}
