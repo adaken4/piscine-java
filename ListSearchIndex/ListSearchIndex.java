f6f0e93cf8e5fbf0cfffbad9d1f785615afd4a38
kada <adakennedy6@gmail.com>
2025-11-18 10:09:51 +0300

    fix: add ListSearchIndex exercise

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -46,6 +46,9 @@ import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null) {
+            return null;
+        }
         for (int i = list.size() - 1; i >= 0; i--) {
             if (list.get(i).equals(value)) {
                 return i;
@@ -55,6 +58,9 @@ public class ListSearchIndex {
     }
 
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null) {
+            return null;
+        }
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
                 return i;
@@ -65,6 +71,9 @@ public class ListSearchIndex {
 
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         List<Integer> indexes = new ArrayList<>();
+        if (list == null) {
+            return indexes;
+        }
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
                 indexes.add(i);
