b9b50c0a0a99f07673e3047b70a139dbcdfb0250
kada <adakennedy6@gmail.com>
2025-11-18 10:37:12 +0300

    fix: add SortList exercise

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -38,16 +38,20 @@
 // [98, 54, 18, 15, 14, 14, 12, 1, -1]
 // $ 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SortList {
     public static List<Integer> sort(List<Integer> list) {
-        list.sort(Integer::compareTo);
-        return list;
+        List<Integer> sorted = new ArrayList<>(list);
+        Collections.sort(sorted);
+        return sorted;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        list.sort((a, b) -> b.compareTo(a));
-        return list;
+        List<Integer> sorted = new ArrayList<>(list);
+        sorted.sort((a, b) -> b.compareTo(a));
+        return sorted;
     }
 }
