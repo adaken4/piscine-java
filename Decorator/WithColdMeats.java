52428c41fbb4d906d9baf3862018499dcbbdcb6e
kada <adakennedy6@gmail.com>
2025-11-25 08:56:33 +0300

    feat: add Decorator exercise

---
+++ Decorator/WithColdMeats.java
@@ -0,0 +1,21 @@
+public class WithColdMeats extends RacletteDecorator {
+
+    public WithColdMeats(Raclette raclette) {
+        super(raclette);
+    }
+
+    @Override
+    public int getCalories() {
+        return super.getCalories() + 350;
+    }
+
+    @Override
+    public String getIngredients() {
+        return super.getIngredients() + ", charcuterie";
+    }
+
+    @Override
+    public String toString() {
+        return  getIngredients() + " pour " + getCalories() + " calories";
+    }
+}
