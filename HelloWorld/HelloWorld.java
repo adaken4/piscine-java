26927a9e8331d74c71e419134da856b685509beb
Kennedy Ada <adakennedy6@gmail.com>
2025-12-01 20:55:10 +0300

    docs: add Exercise instructions

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,3 +1,30 @@
+// Instructions
+
+// Create a file HelloWorld.java in a directory named HelloWorld.
+
+// Write a function helloWorld that returns the string 'Hello World !'.
+
+// Expected Functions
+// public class HelloWorld {
+//     public static String helloWorld() {
+//         // your code here
+//     }
+// }
+// Usage
+// Here is a possible ExerciseRunner.java to test your function :
+
+// public class ExerciseRunner {
+//     public static void main(String[] args) {
+//         System.out.println(HelloWorld.helloWorld());
+//     }
+// }
+// and its output :
+
+// $ javac *.java -d build
+// $ java -cp build ExerciseRunner
+// Hello World !
+// $
+
 public class HelloWorld {
     public static String helloWorld() {
         return "Hello World !";
