79c8ee9a9605ff7a2d7b7471dc3fb32ade2524f9
Kennedy Ada <adakennedy6@gmail.com>
2025-12-01 20:58:24 +0300

    docs: add Exercise instructions

--- IntOperations/IntOperations.java
+++ IntOperations/IntOperations.java
@@ -1,3 +1,72 @@
+// Instructions
+
+// Create a file IntOperations.java.
+
+// Write the following functions:
+
+// addTwoIntegers that returns the sum of two integers passed as parameter.
+
+// substractTwoIntegers that subtracts the second parameter from the first parameter, and returns the result.
+
+// multiplyTwoIntegers that returns the multiplication of two integers passed as parameter.
+
+// divideTwoIntegers that returns the Euclidian division of two integers passed as parameter. We ignore the divide by 0 case.
+
+// Expected Functions
+// public class IntOperations {
+//     public static int addTwoIntegers(int a, int b) {
+//         // your code here
+//     }
+//     public static int subtractTwoIntegers(int a, int b) {
+//         // your code here
+//     }
+//     public static int multiplyTwoIntegers(int a, int b) {
+//         // your code here
+//     }
+//     public static int divideTwoIntegers(int a, int b) {
+//         // your code here
+//     }
+// }
+// Usage
+// Here is a possible ExerciseRunner.java to test your function :
+
+// public class ExerciseRunner {
+//     public static void main(String[] args) {
+//         System.out.println("Add");
+//         System.out.println(IntOperations.addTwoIntegers(1, 2));
+//         System.out.println(IntOperations.addTwoIntegers(15, 68));
+
+//         System.out.println("Subtract");
+//         System.out.println(IntOperations.subtractTwoIntegers(4, 1));
+//         System.out.println(IntOperations.subtractTwoIntegers(10, 23));
+
+//         System.out.println("Multiply");
+//         System.out.println(IntOperations.multiplyTwoIntegers(3, 6));
+//         System.out.println(IntOperations.multiplyTwoIntegers(12, 11));
+
+//         System.out.println("Divide");
+//         System.out.println(IntOperations.divideTwoIntegers(8, 2));
+//         System.out.println(IntOperations.divideTwoIntegers(13, 4));
+//     }
+// }
+// and its output :
+
+// $ javac *.java -d build
+// $ java -cp build ExerciseRunner
+// Add
+// 3
+// 83
+// Subtract
+// 3
+// -13
+// Multiply
+// 18
+// 132
+// Divide
+// 4
+// 3
+// $
+
 public class IntOperations {
     public static int addTwoIntegers(int a, int b) {
         return a + b;
