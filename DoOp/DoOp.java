97e17fb106612a8889f597a7a40bf5ed9933497e
kada <adakennedy6@gmail.com>
2025-12-02 12:41:13 +0300

    docs: add Exercise instructions

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,3 +1,42 @@
+// Instructions
+
+// Create a file named DoOp.java.
+
+// Write a function operate that returns the result of the given arithmetic operation specified in the parameters. The arguments should be passed in the following order:
+
+// The first argument is the left operand
+
+// The second argument is the operation sign
+
+// The third argument is the right operand
+
+// Expected Functions
+// public class DoOp {
+//     public static String operate(String[] args) {
+//         // your code here
+//     }
+// }
+// Usage
+// Here is a possible ExerciseRunner.java to test your function :
+
+// public class ExerciseRunner {
+//     public static void main(String[] args) {
+//         System.out.println(DoOp.operate(new String[]{"1","+","2"}));
+//         System.out.println(DoOp.operate(new String[]{"1","-","1"}));
+//         System.out.println(DoOp.operate(new String[]{"1","%","0"}));
+//         System.out.println(DoOp.operate(args));
+//     }
+// }
+// and its output :
+
+// $ javac *.java -d build
+// $ java -cp build ExerciseRunner
+// 3
+// 0
+// Error
+// it depend on the input.
+// $
+
 public class DoOp {
     public static String operate(String[] args) {
         if (args.length != 3) {
